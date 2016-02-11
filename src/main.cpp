c5e98de2fad2cb21f8ff22da1defe6cf14fc2aa8
Chris Beck <beck.ct@gmail.com>
2016-02-11 18:06:10 -0500

    reduce and reformat

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,17 @@
-// dispatch
-
-template <typename T, T t> struct dispatch_helper;
-
 template <typename T, typename... Args>
 using member_callback_t = int (T::*)(void *, Args...);
 
-//
+template <typename T, T t> struct dispatch_helper;
 
 template <typename T, typename ... Args, member_callback_t<T, Args...> target_func>
 struct dispatch_helper<member_callback_t<T, Args...>, target_func> {
-  static int dispatch(void * L) {
-    return 0;
-  }
+  static int dispatch(void *) { return 0; }
 };
 
 #define DISPATCH(F) &dispatch_helper<decltype(F), (F)>::dispatch
 
-//
-
 struct test {
-  int callback_one(void *, int) {
-    return 0;
-  }
+  int callback_one(void *, int) { return 0; }
 };
 
 typedef int (*desired_sig_t)(void *);
