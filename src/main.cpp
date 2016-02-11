f005f60af7d461a64d8afb67ad95a0cee32e4315
Chris Beck <beck.ct@gmail.com>
2016-02-11 17:44:45 -0500

    fold dispatch into main, remove many unused mock_lua functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include "mock_lua.hpp"
-#include "dispatch.hpp"
+
+template <typename T, T t> struct dispatch_helper;
+
+template <typename T, typename... Args>
+using member_callback_t = int (T::*)(mock_lua_State *, Args...);
+
+//
+
+template <typename T, typename ... Args, member_callback_t<T, Args...> target_func>
+struct dispatch_helper<member_callback_t<T, Args...>, target_func> {
+  static int dispatch(mock_lua_State * L) {
+    return 0;
+  }
+};
+
+#define DISPATCH(F) &dispatch_helper<decltype(F), (F)>::dispatch
+
+//
 
 struct test {
   int callback_one(mock_lua_State *, const char *) {
@@ -8,7 +25,7 @@ struct test {
 };
 
 int main() {
-  mock_lua_State * L = mock_lua_newstate();
+  mock_lua_State * L = 0;
 
   mock_lua_pushcfunction(L, DISPATCH(&test::callback_one));
 }
--- src/mock_lua.hpp
+++ src/mock_lua.hpp
@@ -9,26 +9,6 @@ struct mock_lua_State {
 typedef int (*mock_lua_CFunction)(mock_lua_State *);
 
 /***
- * Create, destroy
- */
-
-inline mock_lua_State * mock_lua_newstate() {
-  return new mock_lua_State();
-}
-
-inline void mock_lua_close(mock_lua_State * L) {
-  delete L;
-}
-
-/***
- * extraspace
- */
-
-inline void * mock_lua_getextraspace(mock_lua_State * L) {
-  return &(L->extraspace);
-}
-
-/***
  * Push c function
  */
 
