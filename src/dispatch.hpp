2f922257ab89608894ff73c1c4499bf804a705f5
Chris Beck <beck.ct@gmail.com>
2016-02-11 17:35:53 -0500

    reduce the test case

--- src/dispatch.hpp
+++ src/dispatch.hpp
@@ -2,56 +2,6 @@
 
 #include "mock_lua.hpp"
 
-#include <cstddef>
-#include <tuple>
-#include <type_traits>
-#include <utility>
-
-/***
- * Utility for manipulating lists of integers
- */
-
-template <std::size_t... Ss>
-struct SizeList {
-  static constexpr std::size_t size = sizeof...(Ss);
-};
-
-// Metafunction Concat: Concatenate two lists
-template <typename L, typename R>
-struct Concat;
-
-template <std::size_t... TL, std::size_t... TR>
-struct Concat<SizeList<TL...>, SizeList<TR...>> {
-  typedef SizeList<TL..., TR...> type;
-};
-
-template <typename L, typename R>
-using Concat_t = typename Concat<L, R>::type;
-
-/***
- * Count_t<n> produces a sizelist containing numbers 0 to n-1.
- */
-template <std::size_t n>
-struct Count {
-  typedef Concat_t<typename Count<n - 1>::type, SizeList<n - 1>> type;
-};
-
-template <>
-struct Count<0> {
-  typedef SizeList<> type;
-};
-
-template <std::size_t n>
-using Count_t = typename Count<n>::type;
-
-/***
- * Utility to cast extraspace pointer to owner type
- */
-template <typename T>
-T*& extra_space_ptr_ref(mock_lua_State * L) {
-  return *static_cast<T**>(mock_lua_getextraspace(L));
-}
-
 /***
  * Helper template which allows to shoe-horn various C++ functions into the required 
  * int (mock_lua_State *) signature so that they can be registered
@@ -69,41 +19,9 @@ using free_callback_t = int (*)(mock_lua_State *, Args...);
 template <typename ... Args, free_callback_t<Args...> target_func>
 struct dispatch_helper<free_callback_t<Args...>, target_func> {
 
-  static int dispatch(mock_lua_State * L) {
-    return invoker<Count_t<sizeof...(Args)>>::dispatch(L);
+  static int dispatch(mock_lua_State *) {
+    return 0;
   }
-  
-private:
-  typedef std::tuple<Args...> tuple_t;
-
-  template <typename T> struct invoker;
-
-  template <std::size_t... indices>
-  struct invoker<SizeList<indices...>> {
-
-    static_assert(sizeof...(indices) == sizeof...(Args), "Something is wrong with template parameters");
-
-    static int dispatch(mock_lua_State * L) noexcept {
-
-      tuple_t tup;
-
-      {
-        bool results[sizeof...(indices) + 1] = { try_read_from_stack<indices>(L, std::get<indices>(tup))..., true };
-
-        bool ok = true;
-        for (int i = 0; i < sizeof...(indices); ++i) { ok &= results[i]; }
-        if (!ok) { return -1; }
-      }
-
-      return target_func(L, std::forward<Args>(std::get<indices>(tup))...);
-    }
-
-  private:
-    template <std::size_t index, typename T>
-    static bool try_read_from_stack(mock_lua_State * L, T & t) {
-      return true;
-    }
-  };
 };
 
 /***
@@ -118,8 +36,7 @@ template <typename T, typename ... Args, member_callback_t<T, Args...> target_fu
 struct dispatch_helper<member_callback_t<T, Args...>, target_func> {
 private:
   static int dispatch_from_extraspace(mock_lua_State * L, Args... args) {
-    T & owner = *extra_space_ptr_ref<T>(L);
-    return (owner.*target_func)(L, std::forward<Args>(args)...);
+    return 0;
   }
 
 public:
